1188672d77f37adb5e9b118019802f65a35ce8b8
mininscode <mininscode@gmail.com>
2022-08-03 00:20:55 +0300

    Update database __init__.py: imports od credentials

--- database/__init__.py
+++ database/__init__.py
@@ -2,14 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from config import settings as config
+from config import settings as _settings
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{config.DATABASE_USERNAME}:" \
-                          f"{config.DATABASE_PASSWORD}@postgresserver/" \
-                          f"{config.DATABASE_NAME}"
+DB_NAME = _settings.DATABASE_NAME
+DB_USER = _settings.DATABASE_USERNAME
+DB_PASS = _settings.DATABASE_PASSWORD
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@localhost:5432/{DB_NAME}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
