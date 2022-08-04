a6c2e0a95214c6670676dbc30cf528e11b7ab5ed
mininscode <mininscode@gmail.com>
2022-08-04 17:58:17 +0300

    Update migration settings: add HouseCondition model

--- database/migration_settings.py
+++ database/migration_settings.py
@@ -2,3 +2,4 @@ from database import Base
 from database.users.models import User
 from database.clients.models import Client
 from database.houses.models import House
+from database.conditions.models import HouseCondition
