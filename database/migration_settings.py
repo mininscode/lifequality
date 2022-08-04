520819e9838cad07335a1a6084c37fabbb40c6f9
mininscode <mininscode@gmail.com>
2022-08-05 00:05:01 +0300

    Create migrations

--- database/migration_settings.py
+++ database/migration_settings.py
@@ -3,3 +3,4 @@ from database.users.models import User
 from database.clients.models import Client
 from database.houses.models import House
 from database.conditions.models import HouseCondition
+from database.employees.models import Employee
