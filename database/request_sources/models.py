fe2405258be614f8f629c972d5e022932b67a0cc
mininscode <mininscode@gmail.com>
2022-08-05 10:06:27 +0300

    Update tablename RequestSource model

--- database/request_sources/models.py
+++ database/request_sources/models.py
@@ -5,7 +5,7 @@ from database import Base
 
 
 class RequestSource(Base):
-    __tablename__ = 'sources'
+    __tablename__ = 'request_sources'
 
     id = Column(BigInteger, primary_key=True, index=True)
     name = Column(String(50), nullable=False)
