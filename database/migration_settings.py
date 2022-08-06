7c62f7d9c60fbd54faa7e0c03f53e21c70805603
mininscode <mininscode@gmail.com>
2022-08-06 03:51:47 +0300

    Add migrations rev13, rev14, rev15 and update migrations settings

--- database/migration_settings.py
+++ database/migration_settings.py
@@ -2,5 +2,7 @@ from database import Base
 from database.users.models import User
 from database.clients.models import Client
 from database.houses.models import House
-from database.conditions.models import HouseCondition
+from database.house_conditions.models import HouseCondition
 from database.employees.models import Employee
+from database.request_sources.models import RequestSource
+from database.contractors.models import Contractor
