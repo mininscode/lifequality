5842f8056c8e17539a993f8dc6c4da5718582cf2
mininscode <mininscode@gmail.com>
2022-08-08 16:17:43 +0300

    Create Contract model
    Create Contract model: model, schema, CRUD operations.

---
+++ database/contracts/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, BigInteger, ForeignKey, Integer, Boolean, Date
+
+from database import Base
+
+
+class Contract(Base):
+    __tablename__ = 'contracts'
+
+    id = Column(BigInteger, primary_key=True, index=True)
+    contract_number = Column(Integer, nullable=True)
+    contract_date = Column(Date, nullable=False)
+    expiration_date = Column(Date, nullable=False)
+    contractor_id = Column(Integer, ForeignKey('contractors.id'), \
+                                                nullable=False)
+    house_id = Column(Integer, ForeignKey('houses.id'), nullable=False)
+    is_active = Column(Boolean, nullable=False, default=True)
+
+    contractor = relationship('Contractor', back_populates='contracts')
+    house = relationship('House', back_populates='contracts')
+
