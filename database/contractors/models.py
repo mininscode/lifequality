da842cefdd125edfd35de2e8044f8de6a21a0dea
mininscode <mininscode@gmail.com>
2022-08-08 16:54:20 +0300

    Update Contractor model: add is_emergency_service flag

--- database/contractors/models.py
+++ database/contractors/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, BigInteger, ForeignKey, Integer, String
+from sqlalchemy import Column, BigInteger, ForeignKey, Integer, String, Boolean
 
 from database import Base
 
@@ -13,6 +13,7 @@ class Contractor(Base):
     street = Column(String(100), nullable=False)
     building = Column(Integer, nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    is_emergency_service = Column(Boolean, nullable=False, default=False)
 
     contracts = relationship('Contract', back_populates='contractor')
     user = relationship('User', back_populates='contractors')
